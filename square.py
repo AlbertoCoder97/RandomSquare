7236f67dbce98f69fb7e64420e162d2d03a16c02
AlbertoCoder97 <30498927+AlbertoCoder97@users.noreply.github.com>
2018-10-25 20:57:01 +0200

    Updated program to add contour lines

--- square.py
+++ square.py
@@ -19,9 +19,12 @@ while i < height:
     while j < width:
         n = random.randint(0,1)
         if n > 0:
-            draw.rectangle([i,j,i+h,j+w], fill=0)
+            draw.rectangle([i,j,i+h,j+w], fill="black", outline="black", width = 1)
+        else:
+            draw.rectangle([i,j,i+h,j+w], fill="white", outline="black", width = 1)
         j= j + w    
     i = i + h
-    
-
+ 
+draw.line([width-1,0,width-1,height-1], fill="black") 
+draw.line([0,height-1,width-1,height-1], fill="black") 
 img.save('randomsquare.png')
